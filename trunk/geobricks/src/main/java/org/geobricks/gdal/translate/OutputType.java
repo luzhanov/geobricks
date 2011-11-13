3d0e7c0d3c19f75017e3fca6d7650f3a20b5763c
guido.barbaglia@gmail.com <guido.barbaglia@gmail.com@36a91b09-4f76-553d-3670-4bc972d51c59>
2011-11-13 13:24:00 +0000

    GDAL Translate Maven Site

---
+++ trunk/geobricks/src/main/java/org/geobricks/gdal/translate/OutputType.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * GeoBricks
+ *
+ * Copyright (c) 2011 by Kalimaha
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.geobricks.gdal.translate;
+
+/**
+ * 
+ * @author <a href="mailto:guido.barbaglia@gmail.com">Guido Barbaglia</a> 
+ *
+ */
+public enum OutputType {
+
+	Byte, Int16, UInt16, UInt32, Int32, Float32, Float64, 
+	CInt16, CInt32, CFloat32, CFloat64;
+	
+}
